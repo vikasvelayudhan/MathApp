3befe9ba882d7851f9efdc5c839aee7e4578d90b
Vikas <vikasvelayudhan@gmail.com>
2026-05-11 23:52:44 +0200

    Fix: use relative paths for GitHub Pages subdirectory hosting

--- German/sw.js
+++ German/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mathquest-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'mathquest-v2';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
